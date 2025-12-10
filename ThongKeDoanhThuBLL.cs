6d8e380603a41a60acbcbea2c1e010fc8b2e5e69
Lefi0305 <huongbdno1@gmail.com>
2025-12-10 21:13:39 +0700

    Add files via upload

---
+++ ThongKeDoanhThuBLL.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QLCF_DAL;
+using QLCF_DTO;
+namespace QLCF_BLL
+{
+    public class ThongKeDoanhThuBLL
+    {
+        ThongKeDoanhThuDAL TKDAL = new ThongKeDoanhThuDAL();
+        public bool fullBaK()
+        {
+            return TKDAL.FullBackup();
+        }
+        public bool diffBaK()
+        {
+            return TKDAL.DifferentialBackup();
+        }
+        public bool logBak()
+        {
+            return TKDAL.TransactionLogBackup();
+        }
+        public void BackUp()
+        {
+            TKDAL.BackupDatabase();
+        }
+    }
+}
